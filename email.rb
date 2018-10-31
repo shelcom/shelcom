0a7ef3984ceae72d6fa8ccd1dfb0de4902fb3068
Sergio Shelkovskyi <shelcom96@gmail.com>
2018-10-31 16:19:17 +0200

    new

--- email.rb
+++ email.rb
@@ -1,25 +1,32 @@
+
+
 class Email
-  def initialize(date,from,subject)
-    @date = date
-    @from = from
+  def initialize(subject, headers)
     @subject = subject
+    @headers = headers
   end
- 
- def date
-    @date
-  end
-  
-  def from
-    @from
-  end
-  
+
   def subject
     @subject
   end
+
+  def date
+    @headers[:date]
+  end
+
+  def from
+    @headers[:from]
+  end
 end
-
-email = Email.new( "24-10-2018","Vova","Homework this week")
-
-puts "Date:    #{email.date}"
-puts "From:    #{email.from}"
-puts "Subject: #{email.subject}"
+emails = [
+  Email.new("Homework this week", { :date => "2014-12-01", :from => "Ferdous" }),
+  Email.new("Keep on coding! :)", { :date => "2014-12-01", :from => "Dajana" }),
+  Email.new("Re: Homework this week", { :date => "2014-12-02", :from => "Ariane" })
+]
+emails.each do |email|
+  puts "Date:    #{email.date}"
+  puts "From:    #{email.from}"
+  puts "Subject: #{email.subject}"
+  
+  
+end
---
+++ mailbox.rb
@@ -0,0 +1,27 @@
+require "./email"
+ 
+class Mailbox
+  def initialize(name, emails)
+    @name = name
+    @emails = emails
+  end
+
+  def name
+    @name
+  end
+
+  def emails
+    @emails
+  end
+end
+
+
+mailbox = Mailbox.new("Ruby Study Group", emails)
+
+mailbox.emails.each do |email|
+  puts "Date:    #{email.date}"
+  puts "From:    #{email.from}"
+  puts "Subject: #{email.subject}"
+  
+  puts "#{mailbox.name}, emails"
+end
